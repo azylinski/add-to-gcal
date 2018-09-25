7055e32847d83d611185edbccb9e7d74a4dd52fe
artur_zylinski <artur.zylinski@10clouds.com>
2018-09-25 22:18:35 +0100

    Extract dates

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Add to GCal
 
 Chrome extension: Extract event data from text and open new tab with new, prepopulated event in Google Calendar
+
+Based on:
+* https://github.com/borismasis/send-to-calendar
+
+
+## Install dependencies
+
+```bash
+bower install
+```
--- main.js
+++ main.js
@@ -1,15 +1,39 @@
-function openEventCreationTab(data) {
-  var desc = data.selectionText;
+// YYYYMMDDTHHMMSSZ
+function formatDt(date) {
+  return date.toISOString().slice(0,-5).replace(/[-:]/g, "") + "Z";
+}
+
+
+// https://github.com/wanasit/chrono#parsedcomponents
+// YYYYMMDDTHHMMSSZ/YYYYMMDDTHHMMSSZ
+function timeIntervalStr(parsedComponent) {
+  var start = parsedComponent.start.date();
+  var end = null;
+
+  if (parsedComponent.end) {
+    end = parsedComponent.end.date();
+  } else {
+    end = parsedComponent.start.date();
+    end.setHours(end.getHours() + 1);
+  }
+
+  return formatDt(start) + '/' + formatDt(end);
+}
+
+
+function openEventCreationTab(info) {
+  var desc = info.selectionText;
+  var datetimes = chrono.casual.parse(desc);
 
   var gCalParams = new URLSearchParams({
-    "action" : 'TEMPLATE',
-    // "text" : titleText,
-    // "dates" : startTime + '/' + endTime,
-    // "location" : locationText,
-    "details" : desc,
-    "trp;" : true,
-    'gsessionid' : 'OK',
-    'output' : 'xml'
+    "action": 'TEMPLATE',
+    // "text": titleText,
+    "dates": timeIntervalStr(datetimes[0]),
+    // "location": locationText,
+    "details": desc + "\n\n--\n" + encodeURI(info.pageUrl),
+    "trp;": true,
+    'gsessionid': 'OK',
+    'output': 'xml'
   });
   var url = "https://calendar.google.com/calendar/render?" + gCalParams.toString();
   
